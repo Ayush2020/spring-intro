079c86fb329e4df24d9a8e18892f4dcf728b650b
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-02-22 12:05:57 +0530

    [Ayush] add . hello

--- spring_intro/src/main/java/com/capgemini/spring_intro/controller/MyController.java
+++ spring_intro/src/main/java/com/capgemini/spring_intro/controller/MyController.java
@@ -14,24 +14,5 @@ public class MyController {
     public ResponseEntity<String> getHello(){
         return new ResponseEntity<>("Hello from bridgelabz", HttpStatus.OK);
     }
-
-    @GetMapping("/hello/query")
-    public ResponseEntity<String> getHello(@RequestParam String name){
-        return new ResponseEntity<>("Hello " + name + " from bridgelabz", HttpStatus.OK);
-    }
-
-    @GetMapping("/hello/param/{name}")
-    public ResponseEntity<String> hello(@RequestParam String name){
-        return new ResponseEntity<>("Hello " + name + " from bridgelabz", HttpStatus.OK);
-    }
-
-    @PostMapping("/post/user")
-    public ResponseEntity<UserDto> postUser(@RequestBody User user){
-        return new ResponseEntity<>(DtoMapper.mapToUserDto(user), HttpStatus.ACCEPTED);
-    }
     
-    @PutMapping("/hello/put/{name}")
-    public ResponseEntity<String> getHello(@PathVariable String firstName, @RequestParam String lastName){
-        return new ResponseEntity<>("Hello " + firstName + lastName + " from bridgelabz", HttpStatus.OK);
-    }
 }
